2f24ffea3dc49585cca15c8503c5cd47bd5219a3
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2024-08-18 08:42:02 -0400

    Added recursive function to add the numbers and return the sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)){
+        return "ERROR";
+    }
+    else{
+        if(a > b){
+            return a + sumAll(a-1, b)
+        }
+        else if(b>a){
+            return b + sumAll(a, b-1);
+        }
+        else return b;
+    }
 };
 
 // Do not edit below this line
