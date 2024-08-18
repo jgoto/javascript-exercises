51334058f2cae429699f248cbf3e6b75ad779c40
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2024-08-18 08:02:29 -0400

    Added function to remove the values from arrays

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(...theArgs) {
+    const data = theArgs[0]; // Expectation is that index 0 is always an array of the data
+    const itemsToRemove = theArgs.shift();
+    for(let i = 0; i < data.length; i++)
+    {
+        for(let j = 0; j < theArgs.length; j++){
+            if (data[i] === theArgs[j]){
+                data.splice(i,1);
+                if(i>0)  // Need this because we skip the new i value after the splice
+                    i-=1;
+            }
+        }        
+    }
+    return data
 };
 
 // Do not edit below this line
