9016e0e456cc031182a13c88142e690cc64f9b28
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2024-08-17 21:38:19 -0400

    Added function to repeat the string and an error condition in case a negative count is supplied

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(textInput, repeatCount) {
+    let textOutput = "";
+    if(repeatCount<0)
+        return "ERROR";
+    for(let i = 0; i < repeatCount; i++){
+        textOutput += textInput;
+    }
+    return textOutput;
 };
 
 // Do not edit below this line
